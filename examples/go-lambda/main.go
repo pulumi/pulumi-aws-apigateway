7604c1ac9ff3a5c6a3409274ebbc9eebc46d7823
Daniel Bradley <daniel@pulumi.com>
2021-11-16 14:40:37 +0000

    New Go lambda example
    - Configure Go VSCode settings.

---
+++ examples/go-lambda/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	apigateway "github.com/pulumi/pulumi-aws-apigateway/sdk/go/apigateway"
+	"github.com/pulumi/pulumi-aws/sdk/v4/go/aws/lambda"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		helloHandler, err := lambda.NewFunction(ctx, "hello-handler", &lambda.FunctionArgs{
+			Runtime: lambda.RuntimePython3d8,
+			Code: pulumi.NewAssetArchive(map[string]interface{}{
+				".": pulumi.NewFileArchive("./handler"),
+			}),
+			Handler: pulumi.String("handler.handler"),
+		})
+		if err != nil {
+			return err
+		}
+
+		getMethod := apigateway.MethodGET
+		restAPI, err := apigateway.NewRestAPI(ctx, "api", &apigateway.RestAPIArgs{
+			Routes: []apigateway.RouteArgs{
+				{
+					Path:         "/",
+					Method:       &getMethod,
+					EventHandler: helloHandler,
+				},
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		ctx.Export("url", restAPI.Url)
+		return nil
+	})
+}
