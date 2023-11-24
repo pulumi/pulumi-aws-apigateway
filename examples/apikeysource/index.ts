fdc0898aaa0e1ca7ae49c6bdd8a5dccbd63cec94
Luke Hoban <lukehoban@gmail.com>
2023-11-24 10:55:12 -0500

    Fix support for apiKeySource

---
+++ examples/apikeysource/index.ts
@@ -0,0 +1,38 @@
+// Copyright 2023, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import * as aws from "@pulumi/aws";
+import * as apigateway from "@pulumi/aws-apigateway";
+import * as data from "./data";
+
+const f = new aws.lambda.CallbackFunction("f", {
+  callback: async (ev, ctx) => {
+    console.log(JSON.stringify(ev));
+    return {
+      statusCode: 200,
+      body: "goodbye",
+    };
+  },
+});
+
+const api = new apigateway.RestAPI("authorizer-api", {
+  apiKeySource: data.apikeysource,
+  routes: [{
+    path: "/",
+    method: "GET",
+    eventHandler: f,
+  }],
+}, { version: "" });
+
+export const apiKeySource = api.api.apiKeySource;
--- examples/examples_nodejs_test.go
+++ examples/examples_nodejs_test.go
@@ -1,13 +1,14 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-//go:build nodejs || all
-// +build nodejs all
 
 package examples
 
 import (
-	"github.com/pulumi/pulumi/pkg/v3/testing/integration"
+	"path"
 	"path/filepath"
 	"testing"
+
+	"github.com/pulumi/pulumi/pkg/v3/testing/integration"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSimpleTs(t *testing.T) {
@@ -15,6 +16,26 @@ func TestSimpleTs(t *testing.T) {
 	test := getJSBaseOptions(t).
 		With(integration.ProgramTestOptions{
 			Dir: filepath.Join(getCwd(t), "simple"),
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccApiKeySource(t *testing.T) {
+	test := getBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir:     path.Join(getCwd(t), "./apikeysource"),
+			Verbose: true,
+			EditDirs: []integration.EditDir{
+				{
+					Dir:      "./apikeysource/step2",
+					Additive: true,
+					ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
+						apiKeySource := stack.Outputs["apiKeySource"].(string)
+						assert.Equal(t, "AUTHORIZER", apiKeySource)
+					},
+				},
+			},
 		})
 
 	integration.ProgramTest(t, &test)
