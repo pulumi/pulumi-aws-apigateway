6a13b9a78fcf3a50c8144bc5e7f92102e6667974
Piers Karsenbarg <piers@pulumi.com>
2023-12-14 09:28:58 +0000

    Putting back Venelin's work

--- provider/pkg/test-programs/simple/index.ts
+++ provider/pkg/test-programs/simple/index.ts
@@ -8,14 +8,17 @@ const config = new pulumi.Config()
 const useSwaggerSpec = config.getBoolean("useSwaggerSpec")
 const useBinaryMediaType = config.getBoolean("useBinaryMediaType")
 
-const lambda = new aws.lambda.CallbackFunction("f", {
-  callback: async (ev: any, ctx: any) => {
-    console.log(JSON.stringify(ev));
-    return {
-      statusCode: 200,
-      body: "Hello world",
-    };
-  },
+const role = new aws.iam.Role("role", {
+  assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({ Service: "lambda.amazonaws.com" }),
+});
+
+const lambda = new aws.lambda.Function("lambda", {
+  role: role.arn,
+  code: new pulumi.asset.AssetArchive({
+    ".": new pulumi.asset.FileArchive("./handler"),
+  }),
+  runtime: "python3.8",
+  handler: "handler.handler",
 });
 
 const api = new apigateway.RestAPI("api", {
