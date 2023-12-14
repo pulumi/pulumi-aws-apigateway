e2289d47f0564206598bdb5fb3b64ce043d47705
Piers Karsenbarg <piers@pulumi.com>
2023-12-14 09:03:47 +0000

    Using callbackfunction instead of python handler

--- provider/pkg/test-programs/simple/index.ts
+++ provider/pkg/test-programs/simple/index.ts
@@ -8,17 +8,14 @@ const config = new pulumi.Config()
 const useSwaggerSpec = config.getBoolean("useSwaggerSpec")
 const useBinaryMediaType = config.getBoolean("useBinaryMediaType")
 
-const role = new aws.iam.Role("role", {
-  assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({ Service: "lambda.amazonaws.com" }),
-});
-
-const lambda = new aws.lambda.Function("lambda", {
-  role: role.arn,
-  code: new pulumi.asset.AssetArchive({
-    ".": new pulumi.asset.FileArchive("./handler"),
-  }),
-  runtime: "python3.8",
-  handler: "handler.handler",
+const lambda = new aws.lambda.CallbackFunction("f", {
+  callback: async (ev: any, ctx: any) => {
+    console.log(JSON.stringify(ev));
+    return {
+      statusCode: 200,
+      body: "Hello world",
+    };
+  },
 });
 
 const api = new apigateway.RestAPI("api", {
