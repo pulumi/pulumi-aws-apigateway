61ff6d518e49a64db50697b33aa8a17a9506cc68
Ramon Quitales <ramon@pulumi.com>
2025-01-17 16:05:01 -0800

    chore: move ts provider code to reporoot/aws-apigateway and generate embed schema

--- provider/cmd/pulumi-gen-aws-apigateway/main.go
+++ provider/cmd/pulumi-gen-aws-apigateway/main.go
@@ -20,6 +20,10 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
+
+	"github.com/pulumi/pulumi-aws-apigateway/provider/v2/pkg/version"
+	"github.com/spf13/cobra"
 )
 
 const Tool = "pulumi-gen-aws-apigateway"
@@ -35,69 +39,105 @@ const (
 	Schema Language = "schema"
 )
 
-func main() {
-	printUsage := func() {
-		fmt.Printf("Usage: %s <language> <out-dir> <root-pulumi-aws-apigateway-dir> [schema-file] [version]\n", os.Args[0])
-	}
-
-	args := os.Args[1:]
-	if len(args) < 2 {
-		printUsage()
-		os.Exit(1)
-	}
-
-	// Note: outdir is not required since gen-sdk will create the folders from root. We keep it here to keep the same signature as the other providers.
-	language, _ := Language(args[0]), args[1]
-
-	var schemaFile string
-	var version string
-	var base string
-	if language != Schema {
-		if len(args) < 5 {
-			printUsage()
-			os.Exit(1)
-		}
-		base, schemaFile, version = args[2], args[3], args[4]
-	} else if len(args) >= 3 {
-		version = args[2]
-	}
-
-	switch language {
-	case Nodejs, DotNet, Go, Python:
-		generateSDK(language, schemaFile, base, version)
-	case Schema:
-		generateSchema()
+func parseLanguage(text string) (Language, error) {
+	switch text {
+	case "dotnet":
+		return DotNet, nil
+	case "go":
+		return Go, nil
+	case "python":
+		return Python, nil
+	case "nodejs":
+		return Nodejs, nil
+	case "schema":
+		return Schema, nil
 	default:
-		panic(fmt.Sprintf("Unrecognized language %q", language))
+		allLangs := []Language{DotNet, Go, Python, Schema, Nodejs}
+		allLangStrings := []string{}
+		for _, lang := range allLangs {
+			allLangStrings = append(allLangStrings, string(lang))
+		}
+		all := strings.Join(allLangStrings, ", ")
+		return "", fmt.Errorf(`Invalid language: %q, supported values include: %s`, text, all)
 	}
 }
 
-func generateSDK(lang Language, schemaPath, base, version string) {
+func rootCmd() *cobra.Command {
+	var outDir string
+	cmd := &cobra.Command{
+		Use:   Tool,
+		Short: "Pulumi Package Schema and SDK generator for pulumi-awsx",
+		Args: func(_ *cobra.Command, args []string) error {
+			if len(args) != 1 {
+				return fmt.Errorf("accepts %d arg(s), received %d", 1, len(args))
+			}
+			if _, err := parseLanguage(args[0]); err != nil {
+				return err
+			}
+			return nil
+		},
+		RunE: func(_ *cobra.Command, args []string) error {
+			cwd, err := os.Getwd()
+			if err != nil {
+				return err
+			}
+			lang, err := parseLanguage(args[0])
+			if err != nil {
+				return err
+			}
+			return generate(lang, cwd)
+		},
+	}
+	cmd.PersistentFlags().StringVarP(&outDir, "out", "o", "", "Emit the generated code to this directory")
+	return cmd
+}
+
+func generate(language Language, cwd string) error {
+	switch language {
+	case Schema:
+		return generateSchema()
+	case Nodejs, DotNet, Go, Python:
+		schemaPath := filepath.Join(cwd, "provider/pulumi-resource-aws-apigateway/schema.json")
+		return generateSDK(language, schemaPath, cwd, version.Version)
+	default:
+		return fmt.Errorf("unrecognized language %q", language)
+	}
+}
+
+func generateSDK(lang Language, schemaPath, base, version string) error {
 	genSDKCmd := exec.Command(filepath.Join(base, ".pulumi/bin/pulumi"), "package", "gen-sdk", schemaPath, "--language", string(lang), "--version", version)
 	genSDKCmd.Dir = base
 	out, err := genSDKCmd.CombinedOutput()
 	fmt.Println(string(out))
 
 	if err != nil {
-		fmt.Printf("Error generating SDK: %v\n", err)
-		os.Exit(1)
+		return fmt.Errorf("error generating SDK: %w", err)
 	}
+
+	return nil
 }
 
-func generateSchema() {
+func generateSchema() error {
 	// Read contents of schema.yaml file.
 	contents, err := os.ReadFile("schema.yaml")
 	if err != nil {
-		fmt.Printf("Error reading schema.yaml: %v\n", err)
-		os.Exit(1)
+		return fmt.Errorf("error reading schema.yaml: %w", err)
 	}
 
 	// Write contents to schema.json file. Note: the content is still a yaml file as gen-sdk can accept a yaml file.
 	// We call it schema.json to keep the same signature as the other providers.
-	outputPath := "provider/cmd/pulumi-resource-aws-apigateway/schema.json"
+	outputPath := "provider/cmd/pulumi-resource-aws-apigateway/schema.yaml"
 	err = os.WriteFile(outputPath, contents, 0644)
 	if err != nil {
-		fmt.Printf("Error writing schema.json: %v\n", err)
+		return fmt.Errorf("error writing schema.yaml: %w", err)
+	}
+
+	return nil
+}
+
+func main() {
+	if err := rootCmd().Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
